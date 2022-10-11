319744029b1975a518b823496509a921eb308530
Samuel Gaddis <siddag14@live.com>
2022-10-11 14:08:33 -0400

    commit tree 9d34385f4e336636aaaa6995a2344bc8c6df164d

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(x){ 
+  if (x <= 400) {
+   return ("This one is on me!");
+  }
+  else if (x <= 2000) {
+    return "That will be twenty bucks."
+  }
+  else if (x <= 2500 ) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(y){
+ 
+  return y === 'NYC' ? "Ok, sounds good." : "No go.";
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
